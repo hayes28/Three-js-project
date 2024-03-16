48ba3fdc317228d6e628c3b2f7c9bd9de61d0c46
hayes28 <heather.hayes918@gmail.com>
2024-03-16 18:29:38 -0500

    Got it

---
+++ Vite-three.js/README.md
@@ -0,0 +1,47 @@
+# Three.js Sphere Demo
+
+This project is a simple, yet captivating demonstration of the power of Three.js for creating immersive 3D web experiences. Following a comprehensive [Three.js crash course for beginners](https://www.youtube.com/watch?v=_OwJV2xL8M8), I developed an interactive 3D sphere rendered beautifully within the browser.
+
+The tutorial guided me through the essentials of Three.js—a high-level JavaScript library that builds on WebGL. It showed how with just a few lines of code, we can create sophisticated 3D graphics that are web-friendly. In this project, I've created a scene, added a geometric sphere, and integrated lighting to give the sphere depth and dimension.
+
+Additionally, a camera is set up to control the perspective, and a WebGL renderer is used to bring the scene to life. The final result is a responsive 3D sphere that's a starting point for more complex Three.js projects and serves as a testament to the library's potential for web-based graphics.
+
+## Features
+
+- Interactive 3D rendering with Three.js
+- Perspective camera setup for realistic depth and viewing angles
+- Ambient and point lighting to enhance visual dynamics
+- Responsive design compatible with high-resolution displays
+
+Feel free to explore the code and embark on your own 3D web adventures!
+
+## Demo of the project
+![Demo Video](threejs.gif)
+
+## Installation
+
+Initialize the project with npm:
+
+```bash
+npm create vite@latest
+```
+follow the prompts to create the project.
+
+Navigate to the project directory:
+
+```bash
+cd my-vite-app
+```
+
+Install the dependencies:
+
+```bash
+npm install three gsap
+```
+
+to run the project:
+
+```bash
+npm run dev
+```
+
--- Vite-three.js/main.js
+++ Vite-three.js/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import './style.css'
+import gsap from 'gsap'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 // Create a scene
@@ -9,6 +10,7 @@ const scene = new THREE.Scene()
 const geometry = new THREE.SphereGeometry(3, 64, 64)
 const material = new THREE.MeshStandardMaterial({
   color: '#00ff83',
+  roughness: 0.4,
 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
@@ -22,6 +24,7 @@ const sizes = {
 // Create a light
 const light = new THREE.PointLight(0xffffff, 70, 100, 1.7)
 light.position.set(0, 10, 10)
+// light.intensity = 1.5
 scene.add(light)
 
 // Create a camera
@@ -34,14 +37,20 @@ const canvas = document.querySelector('.webgl')
 const renderer = new THREE.WebGLRenderer({
   canvas,
 })
+// Set the renderer size
+renderer.setSize(sizes.width, sizes.height)
+// Set the pixel ratio
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.render(scene, camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
+controls.enableZoom = false
+controls.enablePan = false
+controls.autoRotate = true
+controls.autoRotateSpeed = 3
 
-// Set the renderer size
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
 
 // Resize
 window.addEventListener('resize', () => {
@@ -60,7 +69,32 @@ window.addEventListener('resize', () => {
 const loop = () => {
   // Update objects
   mesh.position.y = Math.sin(window.performance.now() / 1000) * 2
+  controls.update()
   renderer.render(scene, camera)
   window.requestAnimationFrame(loop)
 }
 loop()
+
+// Timeline animation
+const tl = gsap.timeline({ defaults: { duration: 1 } })
+tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1, ease: 'elastic' })
+tl.fromTo('nav', { y: -50, opacity: 0 }, { y: 0, opacity: 1, ease: 'back' }, '-=0.5')
+tl.fromTo('.title', { y: -50, opacity: 0 }, { y: 0, opacity: 1, ease: 'back' }, '-=0.5')
+
+// Mouse animation color
+let mouseDown = false
+let rgb = []
+window.addEventListener('mousedown', () => (mouseDown = true))
+window.addEventListener('mouseup', () => (mouseDown = false))
+
+window.addEventListener('mousemove', (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ]
+    // Change the color
+    mesh.material.color.setRGB(...rgb.map((c) => c / 255))
+  }
+})
